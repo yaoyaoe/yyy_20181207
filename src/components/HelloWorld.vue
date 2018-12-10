8fb64eddc0584034b56a1c50331f0edc9b5a3e0a
yi_yao_yao <1157625854@qq.com>
2018-12-10 10:32:08 +0800

    tttt

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <el-button type="danger">危险按钮</el-button>
+    <el-button type="danger">危险按钮1111</el-button>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
